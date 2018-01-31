cc2dc6ba2ceb987748cadfb26bec7e76bdb13c52
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-01-31 22:47:08 +0100

    import new components and add this to app component template

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,22 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatSidenavModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
