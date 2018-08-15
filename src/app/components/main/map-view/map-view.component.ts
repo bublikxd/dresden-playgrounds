83c3846a46e1e74e3998a5d27359930fad663796
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-08-15 23:03:09 +0200

    add pin icon and add markers to map

--- src/app/components/main/map-view/map-view.component.ts
+++ src/app/components/main/map-view/map-view.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Feature } from '../../../models/feature.model';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, marker, Layer, icon, LatLngExpression } from 'leaflet';
 
 @Component({
   selector: 'app-map-view',
@@ -11,17 +11,42 @@ export class MapViewComponent implements OnInit {
 
   @Input() places: Feature[];
 
+  layerOSM = tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' });
+  pinPath = 'assets/map/location-pin.svg';
+
   options = {
-    layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
+    layers: [this.layerOSM],
     zoom: 12,
     center: latLng(51.050407, 13.737262)
   };
 
+  markers: Layer[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    if (this.places && this.places.length !== 0) {
+      this.createMarkers();
+    }
+  }
+
+  createMarkers() {
+    this.places.forEach(place => {
+      const latLong: LatLngExpression = {
+        lat: place.geometry.coordinates[1],
+        lng: place.geometry.coordinates[0]
+      };
+      const newMarker = marker(
+        latLong,
+        {
+          icon: icon({
+            iconSize: [ 25, 41 ],
+            iconAnchor: [ 13, 41 ],
+            iconUrl: this.pinPath,
+          })
+        });
+      this.markers.push(newMarker);
+    });
   }
 
 }
